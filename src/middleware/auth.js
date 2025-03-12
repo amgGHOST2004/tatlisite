ce1166e07ea19fe517fa8f119fc02cce0609f12a
Emir <tatliemirefe3@gmail.com>
2025-03-12 19:33:02 +0300

    items added to home and index

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.header('Authorization').replace('Bearer ', '');
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.adminId = decoded.id;
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Not authenticated' });
-    }
+  try {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.adminId = decoded.id; // Attach admin ID to the request object
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Not authenticated' });
+  }
 };
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,12 +1,9 @@
-// routes/admin.js
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const auth = require('../middleware/auth'); // Import auth middleware
-const Admin = require('../models/Admin'); // Import the Admin model
+const Admin = require('../models/Admin');
 
-// Admin login
 router.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body;
@@ -31,47 +28,6 @@ router.post('/login', async (req, res) => {
     );
 
     res.json({ token, adminId: admin._id });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Check authentication
-router.get('/check-auth', auth, async (req, res) => {
-  try {
-    const admin = await Admin.findById(req.adminId);
-    if (!admin) {
-      return res.status(404).json({ message: 'Admin not found' });
-    }
-    res.status(200).json({ message: 'Authenticated', adminId: admin.id });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Create a new admin (you might want to protect this route or remove it in production)
-router.post('/create', async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    let admin = await Admin.findOne({ username });
-    if (admin) {
-      return res.status(400).json({ message: 'Admin already exists' });
-    }
-
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    admin = new Admin({
-      username,
-      password: hashedPassword,
-    });
-
-    await admin.save();
-
-    res.status(201).json({ message: 'Admin created successfully' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
