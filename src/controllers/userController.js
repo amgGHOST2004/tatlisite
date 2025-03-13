ffaa7cc40032625281745c79255fddd3fab42f1d
Emir <tatliemirefe3@gmail.com>
2025-03-13 17:42:55 +0300

    naber

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require('../models/userModel');
+const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const crypto = require('crypto');
