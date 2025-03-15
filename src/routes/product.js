82ba347fe34a4a1a25d584e7af2461f827e3478f
Emir <tatliemirefe3@gmail.com>
2025-03-15 20:44:41 +0300

    naber

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Product = require('../models/product'); // Küçük harf kullanın
+const Product = require('../controllers/product'); // Küçük harf kullanın
 const router = express.Router();
 
 // Tüm ürünleri getir
